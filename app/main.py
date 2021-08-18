4a3ab9a403c077d0895add30565c0ca7b9c34b6c
EnriquePernia <Epernia96@gmail.com>
2021-08-18 23:32:40 +0200

    Added testing to main

--- app/main.py
+++ app/main.py
@@ -1,9 +1,21 @@
 from fastapi import FastAPI
 
+from fastapi.testclient import TestClient
+
+
 app = FastAPI()
 
 
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
+def read_main():
+    return {"msg": "Hello World"}
+
+#Testing: https://fastapi.tiangolo.com/tutorial/testing/
+client = TestClient(app)
+
+
+def test_read_main():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"msg": "Hello World"}
 
