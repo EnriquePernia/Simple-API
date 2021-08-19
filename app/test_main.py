9cedbe2bd6d58a41a5fdf2f508fb3ab89e415658
EnriquePernia <Epernia96@gmail.com>
2021-08-19 23:15:27 +0200

    Added tests

---
+++ app/test_main.py
@@ -0,0 +1,16 @@
+from fastapi.testclient import TestClient
+from main import app
+
+
+#Testing: https://fastapi.tiangolo.com/tutorial/testing/
+client = TestClient(app)
+
+def test_success():
+    response = client.get("/success")
+    assert response.status_code == 200
+    assert response.json() == {"msg": "Success"}
+
+def test_error():
+    response = client.get("/error")
+    assert response.status_code == 403
+    assert response.json() == {"msg": "Error"}
